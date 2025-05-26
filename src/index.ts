6d0fcf23bb8179c9d4fb1f5a61cb77b84e5deedf
Swarna03700 <swarnadeepghosh441@gmail.com>
2025-05-27 03:29:46 +0530

    feat: add CRUD operations for posts table

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import 'dotenv/config';
 import { generateSlug } from './lib/utils';
 import { posts } from './db/schema/posts';
 import { drizzle } from 'drizzle-orm/neon-http';
+import { eq } from 'drizzle-orm';
 
 const db = drizzle(process.env.DATABASE_URL!);
 
 async function seed(){
-    const title = "My Fourth Post";
+    const title = "My First Post";
     const content = "This is the content of my fourth post.";
     const slug = generateSlug(title);
 
@@ -16,13 +17,22 @@ async function seed(){
         slug
     }
 
-    await db.execute(`SET TIME ZONE 'Asia/Kolkata'`);
+    // await db.execute(`SET TIME ZONE 'Asia/Kolkata'`);
 
-    await db.insert(posts).values(post);
-    console.log("new post created!");
+    // await db.insert(posts).values(post);
+    // console.log("new post created!");
 
-    // const result = await db.select().from(posts);
-    // console.log(result);
+    const result = await db.select().from(posts);
+    console.log(result);
+
+    await db.delete(posts).where(eq(posts.title, title));
+    // console.log("post deleted!");
+
+    await db.update(posts)
+        .set({ content: "Updated content of my first post." })
+        .where(eq(posts.slug, slug));
+    console.log("post updated successfully!");
+
 
     
 }
